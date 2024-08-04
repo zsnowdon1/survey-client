1475c90b5f6b319933796438c99ffef8c57b5f86
Zack Snowdon <zack@Zacks-Air.lan>
2024-08-04 10:26:16 -0400

    Base repo

---
+++ src/main/java/com/voting/survey_client/SurveyClientApplication.java
@@ -0,0 +1,13 @@
+package com.voting.survey_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyClientApplication.class, args);
+	}
+
+}
