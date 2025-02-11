ee0737242e30496cb122b95d5821c3aea3de2dc5
zsnowdon <zacksnowdon5@gmail.com>
2025-02-10 19:52:08 -0500

    Added props for k8s

--- src/main/java/com/voting/survey_client/dao/impl/SurveyDaoImpl.java
+++ src/main/java/com/voting/survey_client/dao/impl/SurveyDaoImpl.java
@@ -1,10 +1,11 @@
-package com.voting.survey_client.dao;
+package com.voting.survey_client.dao.impl;
 
+import com.voting.survey_client.dao.SurveyDao;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class SurveyDaoImpl implements SurveyDao{
+public class SurveyDaoImpl implements SurveyDao {
 
     private final JdbcTemplate jdbcTemplate;
 
