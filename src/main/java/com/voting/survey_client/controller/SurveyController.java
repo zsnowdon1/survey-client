3e490a3b55b4693222d5ec336679599259bae2e3
zsnowdon <zacksnowdon5@gmail.com>
2025-02-17 12:31:13 -0500

    Changed api route

--- src/main/java/com/voting/survey_client/controller/SurveyController.java
+++ src/main/java/com/voting/survey_client/controller/SurveyController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/client/surveys")
+@RequestMapping("/api/client/surveys")
 @CrossOrigin
 public class SurveyController {
 
